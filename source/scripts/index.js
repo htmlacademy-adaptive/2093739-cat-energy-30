ad64a08ebec0e9a0c06dfec5569d1137d10624c9
Nibelung86 <kitnibelung@gmail.com>
2024-01-27 17:23:55 +0300

    index full

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -17,79 +17,77 @@ navButton.addEventListener('click', function () {
 
 let slider = document.querySelector('.slider');
 let before = document.querySelector('.slider__before');
-let beforeImage = before.querySelector('img');
+let beforeImage = document.querySelector('.slider__before-image');
 let change = document.querySelector('.slider__button-curtain');
 let body = document.body;
 
-
 let isActive = false;
 
-document.addEventListener('DOMContentLoaded', () => {
-	let width = slider.offsetWidth;
-	beforeImage.style.width = `${width}px`;
-});
+try {
+  let width = slider.offsetWidth;
+  beforeImage.style.width = `${width}px`;
 
-change.addEventListener('mousedown', () => {
-	isActive = true;
-});
+  change.addEventListener('mousedown', () => {
+    isActive = true;
+  });
 
-body.addEventListener('mouseup', () => {
-	isActive = false;
-});
+  body.addEventListener('mouseup', () => {
+    isActive = false;
+  });
 
-body.addEventListener('mouseleave', () => {
-	isActive = false;
-});
+  body.addEventListener('mouseleave', () => {
+    isActive = false;
+  });
 
-const beforeAfterSlider = (x) => {
-	let shift = Math.max(0, Math.min(x, slider.offsetWidth));
-	before.style.width = `${shift}px`;
-	change.style.left = `${shift}px`;
-};
+  const beforeAfterSlider = (x) => {
+    let shift = Math.max(0, Math.min(x, slider.offsetWidth));
+    before.style.width = `${shift}px`;
+    change.style.left = `${shift}px`;
+  };
 
-const pauseEvents = (e) => {
-	e.stopPropagation();
-	e.preventDefault();
-	return false;
-};
+  const pauseEvents = (e) => {
+    e.stopPropagation();
+  };
 
-body.addEventListener('mousemove', (e) => {
-	if (!isActive) {
-		return;
-	}
+  body.addEventListener('mousemove', (e) => {
+    if (!isActive) {
+      return;
+    }
 
-	let x = e.pageX;
-	x -= slider.getBoundingClientRect().left;
-	beforeAfterSlider(x);
-	pauseEvents(e);
-});
+    let x = e.pageX;
+    x -= slider.getBoundingClientRect().left;
+    beforeAfterSlider(x);
+    pauseEvents(e);
+  });
 
-change.addEventListener('touchstart', () => {
-	isActive = true;
-});
+  change.addEventListener('touchstart', () => {
+    isActive = true;
+  });
 
-body.addEventListener('touchend', () => {
-	isActive = false;
-});
+  body.addEventListener('touchend', () => {
+    isActive = false;
+  });
 
-body.addEventListener('touchcancel', () => {
-	isActive = false;
-});
+  body.addEventListener('touchcancel', () => {
+    isActive = false;
+  });
 
-body.addEventListener('touchmove', (e) => {
-	if (!isActive) {
-		return;
-	}
+  body.addEventListener('touchmove', (e) => {
+    if (!isActive) {
+      return;
+    }
 
-  let x;
+    let x;
 
-  let i;
-  for (i = 0; i < e.changedTouches.length; i++) {
-  	x = e.changedTouches[i].pageX;
-  }
+    let i;
+    for (i = 0; i < e.changedTouches.length; i++) {
+      x = e.changedTouches[i].pageX;
+    }
 
-  x -= slider.getBoundingClientRect().left;
+    x -= slider.getBoundingClientRect().left;
 
-  beforeAfterSlider(x);
-  pauseEvents(e);
-});
+    beforeAfterSlider(x);
+    pauseEvents(e);
+  });
+} catch(err)
+{};
